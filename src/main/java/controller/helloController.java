6af54099f59695ae5304dbbdea066e3750b44167
koffdarealest <dinhsontung153153@gmail.com>
2024-01-18 02:45:29 +0700

    add testGenCode

--- src/main/java/controller/helloController.java
+++ src/main/java/controller/helloController.java
@@ -7,13 +7,23 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.UUID;
 
 public class helloController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
-        out.println("Hello World");
+
     }
 
+    public static String generateTransactionCode() {
+        UUID uuid = UUID.randomUUID();
+        String randomUUIDString = uuid.toString().replace("-", "");
+        return randomUUIDString.substring(0,12);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateTransactionCode());
+    }
 }
--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -30,5 +30,4 @@ public class HibernateUtil {
         getSessionFactory();
     }
 
-
 }
